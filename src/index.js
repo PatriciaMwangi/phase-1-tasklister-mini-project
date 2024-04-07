9e5d5be250a8b674b8e186b5b434cd2115e83178
patricia <patricia.mwangi@student.moringaschool.com>
2024-04-07 12:56:33 +0300

    e.target

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+let form= document.querySelector("form").addEventListener("submit",function(e){
+  e.preventDefault()
+  let task=e.target.elements["new-task-description"].value
+  console.log(task)
+})
+
+})
